533039884fe7b3377b911f1e8e48e30506bf19b8
tomelsom01 <te28@hotmail.com>
2023-06-04 23:01:45 +0100

    styling

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ModalsController from "./modals_controller"
+application.register("modals", ModalsController)
--- app/javascript/drop.js
+++ app/javascript/drop.js
@@ -1,8 +1,41 @@
 console.log("hello from drop");
 
-let change = document.getElementById("menu-icon");
-let extra = document.getElementsByClassName("nav-menu");
 
-change.addEventListener("click", (event) => {
-  change.classList.remove(extra);
-});
+
+// const openModalButtons =document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons =document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay')
+
+// openModalButton.forEach(button =>{
+//   button.addEventListner('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
+
+// overlay.addEventListener('click', () =>{
+//   const modals = document.querySelectorAll('.modal.active')
+//   modals.forEach(modal => {
+//     closeModal(modal)
+//   })
+// })
+
+// closeModalButton.forEach(button =>{
+//   button.addEventListner('click', () => {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
+
+
+// function openModal(modal){
+//   if (modal == null) return
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
+// }
+
+// function closeModal(modal){
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
+// }

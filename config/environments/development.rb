23195ea51c797c7e877ed369093bba5b74f6a684
tomelsom <te28@hotmail.com>
2024-03-27 21:47:24 +0000

    improved security again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
+    user_name: ENV.fetch['SMTP_USERNAME'],
+    password: ENV.fetch['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
